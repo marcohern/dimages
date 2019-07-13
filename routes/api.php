f1e04893879f0c6c15826d123fdf41a56d4145b1
Marco Hernandez <marcohern@gmail.com>
2019-07-13 09:28:23 -0500

    all paths included

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,14 @@ Route::group([
 ], function () {
     Route::get('/', "DimagesController@api");
 
-    Route::get('/meta/index/{entity}/{identity}/{index?}', 'DimageMetaController@index');
+    Route::get ('/meta/index/{entity}/{identity}/{index?}', 'DimageMetaController@index'  );
+    Route::post('/meta/stage/{session}'                   , 'DimageMetaController@stage'  );
+    Route::post('/meta/confirm/{session}'                 , 'DimageMetaController@confirm');
+
     Route::get('/meta/{entity}/{identity}/{profile}/{densoty}/{index?}', 'DimageMetaController@view_exact');
-    Route::get('/meta/{entity}/{identity}/{index?}', 'DimageMetaController@view');
+    Route::get('/meta/{entity}/{identity}/{index?}'                    , 'DimageMetaController@view');
 
     Route::post('/meta/{entity}/{identity}', "DimageMetaController@store");
-
-    Route::get('/meta/{entity}', 'DimageMetaController@identities');
-    Route::get('/meta', 'DimageMetaController@entities');
+    Route::get('/meta/{entity}'            , 'DimageMetaController@identities');
+    Route::get('/meta'                     , 'DimageMetaController@entities');
 });
