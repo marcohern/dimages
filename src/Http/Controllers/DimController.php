40a0d79d27a7e632e96020a8fb00d8c755f53d5b
Marco Hernandez <marcohern@gmail.com>
2019-07-13 16:59:19 -0500

    indent

--- src/Http/Controllers/DimController.php
+++ src/Http/Controllers/DimController.php
@@ -30,28 +30,28 @@ class DimController extends Controller
     return $image->response($dimage->ext);
   }
 
-    public function full($entity, $identity, $profile, $density, $index=0) {
-      $dimage = $this->dimages->viewExact($entity, $identity, $profile, $density, $index);
-      if ($this->dimages->exists($dimage)) {
-        $path = $this->dimages->file($dimage);
-        $image = IImage::make($path);
+  public function full($entity, $identity, $profile, $density, $index=0) {
+    $dimage = $this->dimages->viewExact($entity, $identity, $profile, $density, $index);
+    if ($this->dimages->exists($dimage)) {
+      $path = $this->dimages->file($dimage);
+      $image = IImage::make($path);
+      return $image->response($dimage->ext);
+    } else {
+      $dsource = $dimage->source();
+      if ($this->dimages->exists($dsource)) {
+        $spath = $this->dimages->file($dsource);
+        $dpath = $this->dimages->file($dimage);
+        $image = IImage::make($spath);
+        $p = config("dimages.profiles.$profile");
+        $d = config("dimages.densities.$density");
+        if (!$p) throw new ImageException("Profile $profile invalid", 0xd9745b9921);
+        if (!$d) throw new ImageException("Density $density invalid", 0xd9745b9922);
+        $w = $p[0]*$d;
+        $h = $p[1]*$d;
+        $image->fit($w, $h)->save($dpath);
         return $image->response($dimage->ext);
-      } else {
-        $dsource = $dimage->source();
-        if ($this->dimages->exists($dsource)) {
-          $spath = $this->dimages->file($dsource);
-          $dpath = $this->dimages->file($dimage);
-          $image = IImage::make($spath);
-          $p = config("dimages.profiles.$profile");
-          $d = config("dimages.densities.$density");
-          if (!$p) throw new ImageException("Profile $profile invalid", 0xd9745b9921);
-          if (!$d) throw new ImageException("Density $density invalid", 0xd9745b9922);
-          $w = $p[0]*$d;
-          $h = $p[1]*$d;
-          $image->fit($w, $h)->save($dpath);
-          return $image->response($dimage->ext);
-        }
       }
-      throw new ImageException("Image not found:$entity/$identity/$profile/$density/$index", 0xd9745b9920);
     }
+    throw new ImageException("Image not found:$entity/$identity/$profile/$density/$index", 0xd9745b9920);
+  }
 }
