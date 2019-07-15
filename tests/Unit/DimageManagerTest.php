f798fb56988a74ec187df8af3b09430750c4fa05
Marco Hernandez <marcohern@gmail.com>
2019-07-14 19:21:19 -0500

    removed orchestra testbench

---
+++ tests/Unit/DimageManagerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Marcohern\Dimages\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Marcohern\Dimages\Lib\Dimages\Dimage;
+use Marcohern\Dimages\Lib\Dimages\DimageName;
+use Marcohern\Dimages\Lib\Dimages\DimageManager;
+
+class DimageManagerTest extends TestCase {
+  protected $dimages;
+
+  protected function setUp() : void {
+    Dimage::boot();
+    $this->dimages = new DimageManager;
+    parent::setUp();
+  }
+
+  protected function tearDown() : void {
+    parent::tearDown();
+    unset($this->dimages);
+    Dimage::shutdown();
+  }
+
+  public function test_url() {
+    $dimage = DimageName::fromFilePath('music/sure-know-something-by-kiss/002.jpg');
+
+    $this->assertSame($this->dimages->url($dimage), '/mh/dim/api/music/sure-know-something-by-kiss/2');
+  }
+}
