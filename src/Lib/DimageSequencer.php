dd53ed70ea5b9001b03142ae032fe99d2fbc601c
Marco Hernandez <marcohern@gmail.com>
2019-07-20 08:50:01 -0500

    sequencer doc and test

--- src/Lib/DimageSequencer.php
+++ src/Lib/DimageSequencer.php
@@ -5,18 +5,41 @@ namespace Marcohern\Dimages\Lib;
 use Illuminate\Support\Facades\Storage;
 use Marcohern\Dimages\Lib\DimageConstants;
 
+/**
+ * Generate an index sequence for images
+ */
 class DimageSequencer {
 
+  /**
+   * Storage scope
+   */
   protected $scope;
+
+  /**
+   * Sub folder where sequences are stored
+   */
   protected $subdir;
+
+  /**
+   * File name of a sequence
+   */
   protected $filename;
 
+  /**
+   * Constructor
+   * 
+   * @param $entity Entity
+   * @param $identity Identity
+   */
   public function __construct($entity, $identity) {
     $this->scope = DimageConstants::FSSCOPE;
     $this->subdir = DimageConstants::SEQDIR;
     $this->filename = "$entity.$identity.id";
   }
 
+  /**
+   * The the current value of a sequence.
+   */
   public function get() {
     $path = "{$this->subdir}/{$this->filename}";
     $disk = Storage::disk($this->scope);
@@ -26,18 +49,31 @@ class DimageSequencer {
     return 0;
   }
 
+  /**
+   * Set the value of a sequence.
+   * 
+   * @param $n sequence value
+   */
   public function put($n) {
     $path = "{$this->subdir}/{$this->filename}";
     $disk = Storage::disk($this->scope);
     $disk->put($path, $n);
   }
 
-  public function next() {
+  /**
+   * Get the next value in the sequence
+   * 
+   * @return int next value in sequence
+   */
+  public function next() : int {
     $next = $this->get();
     $this->put($next+1);
     return $next;
   }
 
+  /**
+   * Delete the sequence file
+   */
   public function drop() {
     $path = "{$this->subdir}/{$this->filename}";
     $disk = Storage::disk($this->scope);
--- tests/Unit/DimageSequencerTest.php
+++ tests/Unit/DimageSequencerTest.php
@@ -5,16 +5,57 @@ namespace Marcohern\Dimages\Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
+
+use Marcohern\Dimages\Lib\DimageSequencer;
 
 class DimageSequencerTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+  public function test_get() {
+    Storage::fake('dimages');
+    Storage::disk('dimages')->put('seqs/test.image.id', 5);
+
+    $sequencer = new DimageSequencer('test','image');
+    $this->assertEquals($sequencer->get(), 5);
+
+    $sequencer = new DimageSequencer('othertest','image');
+    $this->assertEquals($sequencer->get(), 0);
+  }
+
+  public function test_put() {
+    Storage::fake('dimages');
+
+    $sequencer = new DimageSequencer('test','image');
+    $sequencer->put(3);
+    $this->assertEquals($sequencer->get(), 3);
+  }
+
+  public function test_next() {
+    Storage::fake('dimages');
+    Storage::disk('dimages')->put('seqs/othertest.image.id', 8);
+
+    $sequencer = new DimageSequencer('test','image');
+    
+    $this->assertEquals($sequencer->next(), 0);
+    $this->assertEquals($sequencer->next(), 1);
+    $this->assertEquals($sequencer->next(), 2);
+
+    $sequencer = new DimageSequencer('othertest','image');
+    $this->assertEquals($sequencer->next(), 8);
+    $this->assertEquals($sequencer->next(), 9);
+    $this->assertEquals($sequencer->next(), 10);
+  }
+
+  public function test_drop() {
+    Storage::fake('dimages');
+    Storage::disk('dimages')->put('seqs/test.image.id', 3);
+
+    Storage::disk('dimages')->assertExists('seqs/test.image.id');
+
+    $sequencer = new DimageSequencer('test','image');
+    $sequencer->drop();
+    
+    Storage::disk('dimages')->assertMissing('seqs/test.image.id');
+
+  }
 }
