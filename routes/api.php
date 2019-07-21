91297b4402426ff08a4592e79add68335e90af9f
Marco Hernandez <marcohern@gmail.com>
2019-07-20 20:42:48 -0500

    removed meta controller

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,6 @@ Route::group([
     'prefix' => DimageConstants::DIMROUTE,
     'middleware' => ['api']
 ], function () {
-    Route::get ('/_meta/_index/{entity}/{identity}/{index?}', 'DimageMetaController@index');
-    Route::get ('/_meta/_sources/{entity}/{identity}'       , 'DimageMetaController@sources');
-    Route::post('/_meta/_switch_index/{entity}/{identity}'  , 'DimageMetaController@switch_index');
-    //Route::post('/_meta/stage/{session}'                   , 'DimageMetaController@stage'  );
-    //Route::post('/_meta/confirm/{session}'                 , 'DimageMetaController@confirm');
-
-    Route::get('/_meta/{entity}/{identity}/{profile}/{density}/{index?}', 'DimageMetaController@view_exact');
-    Route::get('/_meta/{entity}/{identity}/{index?}'                    , 'DimageMetaController@view');
-
-    Route::post  ('/_meta/{entity}/{identity}', "DimageMetaController@store");
-    Route::delete('/_meta/{entity}/{identity}', 'DimageMetaController@destroy');
-
-    Route::get('/_meta/{entity}', 'DimageMetaController@identities');
-    Route::get('/_meta'         , 'DimageMetaController@entities');
-
     Route::get   ('status'                                            , 'DimController@status'     )->name('dim-status');
     Route::post  ('{entity}/{identity}/switch/{source}/with/{target}' , 'DimController@switch'     )->name('dim-switch');
     Route::post  ('{entity}/{identity}/normalize'                     , 'DimController@normalize'  )->name('dim-normalize');
