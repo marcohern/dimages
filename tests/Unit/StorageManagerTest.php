3558f3df2ebdd83b28f98185080e80ae31adb834
Marco Hernandez <marcohern@gmail.com>
2019-08-03 15:28:17 -0500

    refactored storage

--- tests/Unit/StorageManagerTest.php
+++ tests/Unit/StorageManagerTest.php
@@ -14,15 +14,18 @@ use Marcohern\Dimages\Lib\Managers\StorageManager;
 class StorageManagerTest extends TestCase {
 
   protected $sm = null;
+  protected $disk = null;
 
   protected function setUp():void {
     parent::setUp();
     Storage::fake('dimages');
+    $this->disk = Storage::disk('dimages');
     $this->sm = new StorageManager;
   }
 
   protected function tearDown():void {
     unset($this->sm);
+    unset($this->disk);
     parent::tearDown();
   }
 
@@ -36,7 +39,7 @@ class StorageManagerTest extends TestCase {
   }
 
   public function test_exists() {
-    Storage::disk('dimages')->put('_global/games/death-stranding/004/boxart/hdpi.txt','HELLO');
+    $this->disk->put('_global/games/death-stranding/004/boxart/hdpi.txt','HELLO');
 
     $dimage = new DimageFile('games','death-stranding',4,'txt','boxart','hdpi');
 
@@ -44,7 +47,7 @@ class StorageManagerTest extends TestCase {
   }
 
   public function test_content() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
 
     $dimage = new DimageFile('games','death-stranding',4,'txt','boxart','hdpi', 'marcohern@gmail.com');
 
@@ -52,121 +55,121 @@ class StorageManagerTest extends TestCase {
   }
 
   public function test_deleteSingle() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
 
     $dimage = new DimageFile('games','death-stranding',4,'txt','boxart','hdpi', 'marcohern@gmail.com');
 
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
 
     $this->sm->deleteSingle($dimage);
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
   }
 
   public function test_deleteMultiple() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
 
     $dimage1 = DimageFile::fromFilePath('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
     $dimage2 = DimageFile::fromFilePath('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
     $dimage3 = DimageFile::fromFilePath('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
 
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
 
     $this->sm->deleteMultiple([$dimage1, $dimage2, $dimage3]);
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
   }
 
   public function test_deleteIdentity() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
 
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
 
     $this->sm->deleteIdentity('marcohern@gmail.com','games','death-stranding');
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/000/cover/mdpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt');
   }
 
   public function test_deleteStaging() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
 
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/_tmp/abcdefg/004.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/_tmp/abcdefg/000.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/_tmp/abcdefg/002.txt');
+    $this->disk->assertExists('marcohern@gmail.com/_tmp/abcdefg/004.txt');
+    $this->disk->assertExists('marcohern@gmail.com/_tmp/abcdefg/000.txt');
+    $this->disk->assertExists('marcohern@gmail.com/_tmp/abcdefg/002.txt');
 
     $this->sm->deleteStaging('marcohern@gmail.com','abcdefg');
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/004.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/000.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/002.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/004.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/000.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/002.txt');
   }
 
   public function test_deleteStagingForTenants() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('giovanni.castellanos/_tmp/tuvwxyz/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('giovanni.castellanos/_tmp/tuvwxyz/002.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('giovanni.castellanos/_tmp/tuvwxyz/003.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marco/_tmp/hijklmn/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marco/_tmp/hijklmn/001.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marco/_tmp/hijklmn/002.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
+    $this->disk->put('giovanni.castellanos/_tmp/tuvwxyz/000.txt','HELLO DIMAGE');
+    $this->disk->put('giovanni.castellanos/_tmp/tuvwxyz/002.txt','HELLO DIMAGE');
+    $this->disk->put('giovanni.castellanos/_tmp/tuvwxyz/003.txt','HELLO DIMAGE');
+    $this->disk->put('marco/_tmp/hijklmn/000.txt','HELLO DIMAGE');
+    $this->disk->put('marco/_tmp/hijklmn/001.txt','HELLO DIMAGE');
+    $this->disk->put('marco/_tmp/hijklmn/002.txt','HELLO DIMAGE');
 
     $this->sm->deleteStagingForTenants(['marcohern@gmail.com','marco']);
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertExists('giovanni.castellanos/_tmp/tuvwxyz/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertExists('giovanni.castellanos/_tmp/tuvwxyz/002.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertExists('giovanni.castellanos/_tmp/tuvwxyz/003.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertMissing('marco/_tmp/hijklmn/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertMissing('marco/_tmp/hijklmn/001.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->assertMissing('marco/_tmp/hijklmn/002.txt','HELLO DIMAGE');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
+    $this->disk->assertExists('giovanni.castellanos/_tmp/tuvwxyz/000.txt','HELLO DIMAGE');
+    $this->disk->assertExists('giovanni.castellanos/_tmp/tuvwxyz/002.txt','HELLO DIMAGE');
+    $this->disk->assertExists('giovanni.castellanos/_tmp/tuvwxyz/003.txt','HELLO DIMAGE');
+    $this->disk->assertMissing('marco/_tmp/hijklmn/000.txt','HELLO DIMAGE');
+    $this->disk->assertMissing('marco/_tmp/hijklmn/001.txt','HELLO DIMAGE');
+    $this->disk->assertMissing('marco/_tmp/hijklmn/002.txt','HELLO DIMAGE');
   }
 
   public function test_tenants() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('giovanni.castellanos/games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('giovanni.castellanos/games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
 
     $this->assertSame(['giovanni.castellanos','marcohern@gmail.com'], $this->sm->tenants());
   }
 
   public function test_entities() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/video-games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/video-games/death-stranding/000/cover/mdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
 
     $this->assertSame(['games','video-games'], $this->sm->entities('marcohern@gmail.com'));
   }
 
   public function test_identities() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/darksouls-3/000/cover/mdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/darksouls-3/000/cover/mdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
 
     $this->assertSame(['darksouls-3','death-stranding'], $this->sm->identities('marcohern@gmail.com','games'));
   }
 
   public function test_sources() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/000.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002.txt','HELLO DIMAGE');
 
     $this->assertSame(
       [
@@ -178,9 +181,9 @@ class StorageManagerTest extends TestCase {
   }
 
   public function test_profiles() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/boxart/hdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/cover/mdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/cover/mdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/icon/ldpi.txt','HELLO DIMAGE');
 
     $this->assertSame(
       ['boxart','cover','icon',],
@@ -189,9 +192,9 @@ class StorageManagerTest extends TestCase {
   }
 
   public function test_derivatives() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/boxart/hdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/boxart/mdpi.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/002/boxart/ldpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/boxart/mdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/002/boxart/ldpi.txt','HELLO DIMAGE');
 
     $this->assertSame(
       [
@@ -204,37 +207,37 @@ class StorageManagerTest extends TestCase {
   }
 
   public function test_move() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt','HELLO DIMAGE');
 
     $source = DimageFile::fromFilePath('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
     $target = DimageFile::fromFilePath('giovanni.castellanos/games/death-stranding/000/cover/mdpi.txt');
 
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
-    Storage::disk('dimages')->assertMissing('giovanni.castellanos/games/death-stranding/000/cover/mdpi.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertMissing('giovanni.castellanos/games/death-stranding/000/cover/mdpi.txt');
 
     $this->sm->move($source, $target);
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
-    Storage::disk('dimages')->assertExists('giovanni.castellanos/games/death-stranding/000/cover/mdpi.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/004/boxart/hdpi.txt');
+    $this->disk->assertExists('giovanni.castellanos/games/death-stranding/000/cover/mdpi.txt');
   }
 
   public function test_attach() {
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
-    Storage::disk('dimages')->put('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/002.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/000.txt','HELLO DIMAGE');
+    $this->disk->put('marcohern@gmail.com/_tmp/abcdefg/004.txt','HELLO DIMAGE');
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/002.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/000.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/games/death-stranding/004.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/002.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/000.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/games/death-stranding/004.txt');
 
     $this->sm->attach('marcohern@gmail.com','abcdefg','games','death-stranding');
 
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/002.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/000.txt');
-    Storage::disk('dimages')->assertMissing('marcohern@gmail.com/_tmp/abcdefg/004.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/002.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/000.txt');
+    $this->disk->assertMissing('marcohern@gmail.com/_tmp/abcdefg/004.txt');
 
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/002.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/000.txt');
-    Storage::disk('dimages')->assertExists('marcohern@gmail.com/games/death-stranding/004.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/002.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/000.txt');
+    $this->disk->assertExists('marcohern@gmail.com/games/death-stranding/004.txt');
   }
 }
