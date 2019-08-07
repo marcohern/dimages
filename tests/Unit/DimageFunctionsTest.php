495ed0396ddc4b77c4703c315e5d1dc41f1d890b
Marco Hernandez <marcohern@gmail.com>
2019-08-07 00:15:55 -0500

    removed unused code

--- tests/Unit/DimageFunctionsTest.php
+++ tests/Unit/DimageFunctionsTest.php
@@ -22,20 +22,6 @@ class DimageFunctionsTest extends TestCase
     parent::tearDown();
     Dimage::shutdown();
   }
-  public function test_fileNameRegex() {
-    $regex = DimageFunctions::fileNameRegex();
-    $this->assertRegExp($regex, 'tecno/sucks-to-be-you-by-prozzak/123.cover.mdpi.jpeg');
-    $this->assertRegExp($regex, 'tecno/sucks-to-be-you-by-prozzak/123.jpeg');
-  }
-  
-  public function test_urlRegex() {
-    $regex = DimageFunctions::urlRegex();
-    $this->assertRegExp($regex, 'tecno/sucks-to-be-you-by-prozzak/cover/mdpi/123');
-    $this->assertRegExp($regex, 'tecno/sucks-to-be-you-by-prozzak/cover/mdpi/');
-    $this->assertRegExp($regex, 'tecno/sucks-to-be-you-by-prozzak/123');
-    $this->assertRegExp($regex, 'tecno/sucks-to-be-you-by-prozzak/');
-    $this->assertRegExp($regex, 'tecno/sucks-to-be-you-by-prozzak');
-  }
 
   public function test_pad() {
     $this->assertSame(DimageFunctions::pad(1, 1), '1');
