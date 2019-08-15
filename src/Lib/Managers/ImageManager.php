6eb794229ea8115d55485fa8ab954d41f9d537cd
Marco Hernandez <marcohern@gmail.com>
2019-08-14 22:55:55 -0500

    doc Imagemanager

--- src/Lib/Managers/ImageManager.php
+++ src/Lib/Managers/ImageManager.php
@@ -1,31 +1,52 @@
 <?php
 
-
 namespace Marcohern\Dimages\Lib\Managers;
 
 use Intervention\Image\ImageManagerStatic as IImage;
 
-use Marcohern\Dimages\Lib\DimageFile;
 use Marcohern\Dimages\Lib\Managers\StorageManager;
+use Marcohern\Dimages\Lib\DimageFile;
 use Marcohern\Dimages\Lib\Lockable;
 use Marcohern\Dimages\Lib\Factory;
 
 use Marcohern\Dimages\Exceptions\DimageNotFoundException;
 use Marcohern\Dimages\Exceptions\DimageOperationInvalidException;
 
+/**
+ * Loads and Manipulates images
+ */
 class ImageManager {
 
   use Lockable;
 
+  /**
+   * Storage manager
+   */
   protected $sm;
-  protected $fs;
 
+  /**
+   * Factory
+   */
+  protected $factory;
+
+  /**
+   * Constructor
+   * @param Marcohern\Dimages\Lib\Managers\StorageManager $sm Storage manager
+   * @param Marcohern\Dimages\Lib\Factory $factory Factory
+   */
   public function __construct(StorageManager $sm, Factory $factory) {
     $this->sm = $sm;
     $this->factory = $factory;
   }
 
-  public function sources($tenant, $entity, $identity) {
+  /**
+   * Return sources as array of DimageFile
+   * @param string $tenant Tenant
+   * @param string $entity Entity
+   * @param string $identity Identity
+   * @return array list of DimageFile
+   */
+  public function sources(string $tenant, string $entity, string $identity): array {
     $files = $this->sm->sources($tenant, $entity, $identity);
     $dimages = [];
     foreach ($files as $file) {
@@ -34,6 +55,14 @@ class ImageManager {
     return $dimages;
   }
 
+  /**
+   * Return the source DimageFile
+   * @param string $tenant Tenant
+   * @param string $entity Entity
+   * @param string $identity Identity
+   * @param int $index Index
+   * @return Marcohern\Dimages\Lib\DimageFile DimageFile found
+   */
   public function source(string $tenant, string $entity, string $identity, int $index=0):DimageFile {
     $files = $this->sm->sources($tenant, $entity, $identity);
     foreach ($files as $file) {
@@ -45,10 +74,21 @@ class ImageManager {
     throw new DimageNotFoundException("Source Image Not found: $tenant/$entity/$identity/$index", 0xd9745b9923);
   }
 
+  /**
+   * Find and return the specified derived image. If not found, then find and return the source.
+   * 
+   * @param string $tenant Tenant
+   * @param string $entity Entity
+   * @param string $identity Identity
+   * @param string $profile Profile
+   * @param string $density Density
+   * @param int $index Index
+   * @return Marcohern\Dimages\Lib\DimageFile DimageFile found
+   */
   protected function derivativeOrSource(
     string $tenant, string $entity, string $identity,
     string $profile, string $density, int $index=0
-  ) : DimageFile {
+  ): DimageFile {
     $files = $this->sm->derivatives($tenant, $entity, $identity, $index, $profile);
     foreach ($files as $file) {
       $target = $this->factory->dimageFileFromPath($file);
@@ -68,7 +108,14 @@ class ImageManager {
     throw new DimageNotFoundException("Dimage Not found: $tenant/$entity/$identity/$index", 0xd9745b9923);
   }
 
-  protected function generate(DimageFile $source, $profile, $density) {
+  /**
+   * Generate a derived dimage file from a source.
+   * @param Marcohern\Dimages\Lib\DimageFile $source Source image
+   * @param string $profile Desired profile
+   * @param string $density Desired density
+   * @return Marcohern\Dimages\Lib\DimageFile Generated image
+   */
+  protected function generate(DimageFile $source, string $profile, string $density): DimageFile {
     if (!$this->sm->exists($source))
       throw new DimageNotFoundException("Source Not found", 0xd9745b9923);
     if (!$source->isSource())
@@ -88,9 +135,19 @@ class ImageManager {
     return $target;
   }
 
+  /**
+   * Derive an image. If derivative is not found, then generate from source.
+   * @param string $tenant Tenant
+   * @param string $entity Entity
+   * @param string $identity Identity
+   * @param string $profile Profile
+   * @param string $density Density
+   * @param int $index Index
+   * @return Marcohern\Dimages\Lib\DimageFile Derivative image file
+   */
   public function get(
     string $tenant, string $entity, string $identity,
-    string $profile, string $density, int $index=0) : DimageFile
+    string $profile, string $density, int $index=0): DimageFile
   {
     $dimage = $this->derivativeOrSource($tenant, $entity, $identity, $profile, $density, $index);
     if ($dimage->isDerived()) return $dimage;
@@ -112,7 +169,11 @@ class ImageManager {
     return $derived;
   }
 
-  public function session() {
+  /**
+   * Generate a random session id. Usefull for staging uploads.
+   * @return string Session Id
+   */
+  public function session(): string {
     $template = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
     $tlen = strlen($template);
     $string = '';
