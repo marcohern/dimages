0c15c5667ba8be3ef80cd18aeec84f13407178ff
Marco Hernandez <marcohern@gmail.com>
2019-08-17 23:35:13 -0500

    redis Controller Methods

--- src/Http/Controllers/DimageController.php
+++ src/Http/Controllers/DimageController.php
@@ -24,35 +24,6 @@ class DimageController extends Controller {
     $this->sm = $sm;
   }
 
-  public function status() {
-    return [
-      'success' => true,
-      'xFile' => Dimage::xFile()
-    ];
-  }
-
-  public function session() {
-    return [
-      'session' => $this->im->session()
-    ];
-  }
-
-  public function tenants() {
-    return $this->sm->tenants();
-  }
-
-  public function entities(string $tenant) {
-    return $this->sm->entities($tenant);
-  }
-
-  public function identities(string $tenant, string $entity) {
-    return $this->sm->identities($tenant, $entity);
-  }
-
-  public function sources(string $tenant, string $entity, string $identity) {
-    return $this->im->sources($tenant, $entity, $identity);
-  }
-
   public function source(string $tenant, string $entity, string $identity, int $index = 0) {
     $dimage = $this->im->source($tenant, $entity, $identity, $index);
     $content = $this->sm->content($dimage);
@@ -77,18 +48,10 @@ class DimageController extends Controller {
     $dimage = $this->sm->attach($tenant, $session, $entity, $identity);
   }
 
-  public function switch(string $tenant, string $entity, string $identity, int $source, int $target) {
-    $this->sm->switchIndex($tenant, $entity, $identity, $source, $target);
-  }
-
   public function derive(string $tenant, string $entity, string $identity, string $profile, string $density, int $index = 0) {
     $dimage = $this->im->get($tenant, $entity, $identity, $profile, $density, $index);
     $content = $this->sm->content($dimage);
     return IImage::make($content)->response($dimage->ext);
-  }
-
-  public function normalize(string $tenant, string $entity, string $identity) {
-    $this->sm->normalize($tenant, $entity, $identity);
   }
 
   public function update(UploadDimageRequest $request, string $tenant, string $entity, string $identity, int $index) {
--- src/Http/Controllers/DimagesController.php
+++ src/Http/Controllers/DimagesController.php
@@ -16,5 +16,48 @@ use Marcohern\Dimages\Lib\Managers\StorageManager;
 use Marcohern\Dimages\Http\Requests\UploadDimageRequest;
 
 class DimagesController extends Controller {
-  
+  protected $im;
+  protected $sm;
+
+  public function __construct(StorageManager $sm, ImageManager $im) {
+    $this->im = $im;
+    $this->sm = $sm;
+  }
+
+  public function status() {
+    return [
+      'success' => true,
+      'xFile' => Dimage::xFile()
+    ];
+  }
+
+  public function session() {
+    return [
+      'session' => $this->im->session()
+    ];
+  }
+
+  public function switch(string $tenant, string $entity, string $identity, int $source, int $target) {
+    $this->sm->switchIndex($tenant, $entity, $identity, $source, $target);
+  }
+
+  public function normalize(string $tenant, string $entity, string $identity) {
+    $this->sm->normalize($tenant, $entity, $identity);
+  }
+
+  public function sources(string $tenant, string $entity, string $identity) {
+    return $this->im->sources($tenant, $entity, $identity);
+  }
+
+  public function entities(string $tenant) {
+    return $this->sm->entities($tenant);
+  }
+
+  public function identities(string $tenant, string $entity) {
+    return $this->sm->identities($tenant, $entity);
+  }
+
+  public function tenants() {
+    return $this->sm->tenants();
+  }
 }
